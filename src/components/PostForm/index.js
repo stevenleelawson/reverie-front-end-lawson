b325638921934aa56a6e84d67be45647041865a9
Steven Lawson <unluckygamblermusic@gmail.com>
2018-05-19 11:56:20 -0600

    adding post form data to state

--- src/components/PostForm/index.js
+++ src/components/PostForm/index.js
@@ -5,20 +5,89 @@ export default class PostForm extends Component {
   constructor() {
     super();
     this.state = {
-
+      date_added: '',
+      first_active: '',
+      current_name: '',
+      height: '',
+      weight: '',
+      intelliegence_metric: null
     }
   }
+  // handleSubmit = (e) => {
+  //   e.preventDefault()
+  //   this.props.createThought(this.state)
+  //   this.setState({ title: '', body: ''})
+  // }
+
+  handleChange = (e) => {
+    const { name, value} = e.target
+    console.log(name, value)
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ })
+  }
+
   render() {
+    const {
+      date_added,
+      first_active,
+      current_name,
+      height,
+      weight,
+      intelliegence_metric
+    } = this.state;
+
     return (
       <div className='form-container'>
         <h1>Add a new robit</h1>
         <form>
-          <input type='text' placeholder='date_added'/>
-          <input type='text' placeholder='first_active'/>
-          <input type='text' placeholder='current_name'/>
-          <input type='text' placeholder='height'/>
-          <input type='text' placeholder='weight'/>
-          <input type='number' placeholder='intelliegence_metric'/>
+          <input
+            type='text'
+            name='date_added'
+            placeholder='date_added'
+            value= { date_added }
+            onChange={ this.handleChange }
+          />
+          <input
+             type='text'
+             name='first_active'
+             value={ first_active }
+             placeholder='first_active'
+             onChange={ this.handleChange }
+           />
+          <input
+             type='text'
+             name='current_name'
+             value={ current_name }
+             placeholder='current_name'
+             onChange={ this.handleChange }
+           />
+          <input
+             type='text'
+             name='height'
+             placeholder='height'
+             value={ height }
+             onChange={ this.handleChange }
+           />
+          <input
+             type='text'
+             name='weight'
+             placeholder='weight'
+             value={ weight }
+             onChange={ this.handleChange }
+           />
+          <input
+             type='number'
+             name='intelliegence_metric'
+             placeholder='intelliegence_metric'
+             value={ intelliegence_metric }
+             onChange={ this.handleChange }
+           />
           <button type='submit'>Add Robit</button>
         </form>
       </div>
